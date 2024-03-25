9067570e71d82aefd2a25c5d8e35c9b4ea174732
Jaron Rosenau <jaron.rosenau@gmail.com>
2024-03-24 22:22:03 -0700

    Update main.py

--- app/routes/main.py
+++ app/routes/main.py
@@ -12,4 +12,10 @@ def index():
 @app.route('/submit_task', methods=['GET', 'POST'])
 def submit_task():
     form = TaskForm()
-    if form
+    if form.validate_on_submit():
+        task = Task(title=form.title.data, description=form.description.data, author=current_user)
+        db.session.add(task)
+        db.session.commit()
+        flash('Your task has been submitted!', 'success')
+        return redirect(url_for('index'))
+    return render_template('submit_task.html', title='Submit Task', form=form)
