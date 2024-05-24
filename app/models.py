bfed16c5f751c679bfa8b9bd2712cf3195babf07
denuoweb <jaron@rosenau.info>
2024-05-24 07:06:57 -0700

    super user can delete or add admin / users

--- app/models.py
+++ app/models.py
@@ -53,19 +53,20 @@ class User(UserMixin, db.Model):
     is_admin = db.Column(db.Boolean, default=False)
     is_super_admin = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime, default=datetime.now())
-    license_agreed = db.Column(db.Boolean, nullable=False)  # Ensure this field is not nullable
-    user_tasks = db.relationship('UserTask', backref='user', lazy='dynamic')
+    license_agreed = db.Column(db.Boolean, nullable=False)
+    user_tasks = db.relationship('UserTask', backref='user', lazy='dynamic', cascade='all, delete-orphan')
     badges = db.relationship('Badge', secondary=user_badges, lazy='subquery',
-        backref=db.backref('users', lazy=True))
+                             backref=db.backref('users', lazy=True))
     score = db.Column(db.Integer, default=0)
     participated_games = db.relationship('Game', secondary='user_games', lazy='subquery', backref=db.backref('game_participants', lazy=True))
     display_name = db.Column(db.String(100))
-    profile_picture = db.Column(db.String(200))  # Could be a URL or a path
+    profile_picture = db.Column(db.String(200))
     age_group = db.Column(db.String(50))
-    interests = db.Column(db.String(500))  # This could be a comma-separated list or a many-to-many relationship with another table
-    task_likes = db.relationship('TaskLike', backref='user', lazy='dynamic')
+    interests = db.Column(db.String(500))
+    task_likes = db.relationship('TaskLike', backref='user', lazy='dynamic', cascade='all, delete-orphan')
     email_verified = db.Column(db.Boolean, default=False)
-
+    shoutboard_messages = db.relationship('ShoutBoardMessage', backref='user', lazy='dynamic', cascade='all, delete-orphan')
+    task_submissions = db.relationship('TaskSubmission', backref='submitter', lazy='dynamic', cascade='all, delete-orphan')
 
     def generate_verification_token(self, expiration=320000):
         return jwt.encode(
@@ -98,33 +99,31 @@ class Task(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     evidence_url = db.Column(db.String(500))
     enabled = db.Column(db.Boolean, default=True)
-    is_sponsored = db.Column(db.Boolean, default=False, nullable=False)  # Indicates if the task is sponsored and should be pinned
-    verification_type = db.Column(db.String(50))  # Changed from SQLAlchemyEnum to String
+    is_sponsored = db.Column(db.Boolean, default=False, nullable=False)
+    verification_type = db.Column(db.String(50))
     verification_comment = db.Column(db.String(1000), default="")
     game_id = db.Column(db.Integer, db.ForeignKey('game.id', ondelete='CASCADE'))
-    game = db.relationship('Game', back_populates='tasks')  # Ensures bidirectional access
+    game = db.relationship('Game', back_populates='tasks')
     points = db.Column(db.Integer, default='')
     tips = db.Column(db.String(2000), default='', nullable=True)
-    completion_limit = db.Column(db.Integer, default=1)  # Limit for how many times a task can be completed
-    frequency = db.Column(db.String(50), nullable=True)  # Store frequency as a string
+    completion_limit = db.Column(db.Integer, default=1)
+    frequency = db.Column(db.String(50), nullable=True)
     user_tasks = db.relationship('UserTask', back_populates='task', cascade="all, delete", passive_deletes=True)
     category = db.Column(db.String(50), nullable=True)
-    badge_id = db.Column(db.Integer, db.ForeignKey('badge.id'), nullable=True)  # Foreign key to Badge
+    badge_id = db.Column(db.Integer, db.ForeignKey('badge.id'), nullable=True)
     badge = db.relationship('Badge', back_populates='tasks')
     submissions = db.relationship('TaskSubmission', back_populates='task', cascade='all, delete-orphan')
     likes = db.relationship('TaskLike', backref='task', cascade="all, delete-orphan")
 
 Badge.tasks = db.relationship('Task', order_by=Task.id, back_populates='badge')
 
-
 class TaskLike(db.Model):
     __tablename__ = 'task_likes'
     id = db.Column(db.Integer, primary_key=True)
     task_id = db.Column(db.Integer, db.ForeignKey('task.id', ondelete='CASCADE'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
 
     __table_args__ = (db.UniqueConstraint('task_id', 'user_id', name='_task_user_uc'),)
-
 
 class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -188,35 +187,30 @@ game_participants = db.Table('game_participants',
 class ShoutBoardMessage(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String(500), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    timestamp = db.Column(db.DateTime, index=True, default=lambda: datetime.now(utc))  # Use timezone-aware datetime
-    is_pinned = db.Column(db.Boolean, default=False)  # Add this line
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    timestamp = db.Column(db.DateTime, index=True, default=lambda: datetime.now(utc))
+    is_pinned = db.Column(db.Boolean, default=False)
+    likes = db.relationship('ShoutBoardLike', backref='message', cascade="all, delete-orphan")
 
-    user = db.relationship('User', backref='shoutboard_messages')
-    likes = db.relationship('ShoutBoardLike', backref='message', lazy='dynamic')
-
-    
 class ShoutBoardLike(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    message_id = db.Column(db.Integer, db.ForeignKey('shout_board_message.id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    message_id = db.Column(db.Integer, db.ForeignKey('shout_board_message.id', ondelete='CASCADE'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
 
     __table_args__ = (db.UniqueConstraint('message_id', 'user_id', name='_message_user_uc'),)
-
 
 class TaskSubmission(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task_id = db.Column(db.Integer, db.ForeignKey('task.id', ondelete='CASCADE'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
     image_url = db.Column(db.String(500), nullable=True)
     comment = db.Column(db.String(1000), nullable=True)
-    timestamp = db.Column(db.DateTime, index=True, default=lambda: datetime.now(utc))  # Use timezone-aware datetime
+    timestamp = db.Column(db.DateTime, index=True, default=lambda: datetime.now(utc))
     twitter_url = db.Column(db.String(1024), nullable=True)
     fb_url = db.Column(db.String(1024), nullable=True)
 
     task = db.relationship('Task', back_populates='submissions')
-    user = db.relationship('User', backref='task_submissions')
-    
+    user = db.relationship('User', back_populates='task_submissions', overlaps="submitter")
 
 class Sponsor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -228,4 +222,3 @@ class Sponsor(db.Model):
     game_id = db.Column(db.Integer, db.ForeignKey('game.id'), nullable=False)
 
     game = db.relationship('Game', back_populates='sponsors')
-
