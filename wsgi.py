b58c54c1633d03ddba96c71d8b1be56d22213550
denuoweb <jaron@rosenau.info>
2024-06-21 14:19:33 -0700

    fix socketIO

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,9 @@
-# wsgi.py
+from gevent import monkey
+monkey.patch_all()
+
 from app import create_app, socketio
-from gevent import pywsgi
-from geventwebsocket.handler import WebSocketHandler
 
 app = create_app()
 
 if __name__ == "__main__":
-    server = pywsgi.WSGIServer(('0.0.0.0', 5000), app, handler_class=WebSocketHandler)
-    server.serve_forever()
+    socketio.run(app, debug=False)
