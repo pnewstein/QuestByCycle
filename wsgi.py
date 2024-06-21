c84379a2a938f6116fbfb185b579dbfac86e0e92
denuoweb <jaron@rosenau.info>
2024-06-21 06:20:09 -0700

    set up web sockets

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,10 @@
 # wsgi.py
-from app import create_app
+from app import create_app, socketio
+from gevent import pywsgi
+from geventwebsocket.handler import WebSocketHandler
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run()
-
+    server = pywsgi.WSGIServer(('0.0.0.0', 5000), app, handler_class=WebSocketHandler)
+    server.serve_forever()
