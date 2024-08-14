35ab1b6c4ca757abef20b5ebaeb3c545a361d22f
denuoweb <jaron@rosenau.info>
2024-08-14 00:50:48 -0700

    switch join custom game to drop down

--- app/static/js/join_custom_game_modal.js
+++ app/static/js/join_custom_game_modal.js
@@ -8,3 +8,13 @@ function closeJoinCustomGameModal(){
     joinCustomGameModal.style.display = 'none';
     document.body.classList.remove('body-no-scroll');
 }
+function handleGameSelection(selectElement) {
+    const selectedValue = selectElement.value;
+
+    // Open the "Join Custom Game" modal if selected
+    if (selectedValue === 'join_custom_game') {
+        openModal('joinCustomGameModal');
+    } else {
+        window.location.href = selectedValue;
+    }
+}
